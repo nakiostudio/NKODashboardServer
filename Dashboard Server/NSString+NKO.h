3ec5c42746fc11f772f77adcaaa2cdd8e1a89228
Carlos Vidal <carlos@Carlos-iMac.local>
2014-03-07 11:33:21 +0000

    created NSString category

---
+++ Dashboard Server/NSString+NKO.h
@@ -0,0 +1,13 @@
+//
+//  NSString+NKO.h
+//  Dashboard Server
+//
+//  Created by Carlos Vidal on 07/03/2014.
+//  Copyright (c) 2014 nakioStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (NKO)
+
+@end
---
+++ Dashboard Server/NSString+NKO.m
@@ -0,0 +1,13 @@
+//
+//  NSString+NKO.m
+//  Dashboard Server
+//
+//  Created by Carlos Vidal on 07/03/2014.
+//  Copyright (c) 2014 nakioStudio. All rights reserved.
+//
+
+#import "NSString+NKO.h"
+
+@implementation NSString (NKO)
+
+@end
