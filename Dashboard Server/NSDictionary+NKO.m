c21578ec664e51c0ea346657688f9690e1afb5a7
Carlos Vidal <carlos@Carlos-iMac.local>
2014-03-07 11:23:38 +0000

    created NSDictionary category

---
+++ Dashboard Server/NSDictionary+NKO.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+NKO.m
+//  Dashboard Server
+//
+//  Created by Carlos Vidal on 07/03/2014.
+//  Copyright (c) 2014 nakioStudio. All rights reserved.
+//
+
+#import "NSDictionary+NKO.h"
+
+@implementation NSDictionary (NKO)
+
+@end
