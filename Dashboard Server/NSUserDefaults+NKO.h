da903d9965a690b40154384a75b7cf3ee78d5d86
Carlos Vidal <carlos@Carlos-iMac.local>
2014-03-07 13:15:49 +0000

    created user defaults category

---
+++ Dashboard Server/NSUserDefaults+NKO.h
@@ -0,0 +1,13 @@
+//
+//  NSUserDefaults+NKO.h
+//  Dashboard Server
+//
+//  Created by Carlos Vidal on 07/03/2014.
+//  Copyright (c) 2014 nakioStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSUserDefaults (NKO)
+
+@end
---
+++ Dashboard Server/NSUserDefaults+NKO.m
@@ -0,0 +1,13 @@
+//
+//  NSUserDefaults+NKO.m
+//  Dashboard Server
+//
+//  Created by Carlos Vidal on 07/03/2014.
+//  Copyright (c) 2014 nakioStudio. All rights reserved.
+//
+
+#import "NSUserDefaults+NKO.h"
+
+@implementation NSUserDefaults (NKO)
+
+@end
