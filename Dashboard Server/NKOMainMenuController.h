bacf6e8bce83e616bbbd79a023435e67b68c714c
Carlos Vidal <carlos@Carlos-iMac.local>
2014-03-12 13:10:13 +0000

    created main menu controller class

---
+++ Dashboard Server/NKOMainMenuController.h
@@ -0,0 +1,13 @@
+//
+//  NKOMainMenuController.h
+//  Dashboard Server
+//
+//  Created by Carlos Vidal on 12/03/2014.
+//  Copyright (c) 2014 nakioStudio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NKOMainMenuController : NSViewController
+
+@end
---
+++ Dashboard Server/NKOMainMenuController.m
@@ -0,0 +1,13 @@
+//
+//  NKOMainMenuController.m
+//  Dashboard Server
+//
+//  Created by Carlos Vidal on 12/03/2014.
+//  Copyright (c) 2014 nakioStudio. All rights reserved.
+//
+
+#import "NKOMainMenuController.h"
+
+@implementation NKOMainMenuController
+
+@end
